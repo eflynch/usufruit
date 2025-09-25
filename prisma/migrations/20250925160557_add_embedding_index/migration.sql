e20076ca4b94c68443461e115cee1448c892ec2a
Evan Lynch <evan.f.lynch@gmail.com>
2025-09-25 14:47:47 -0400

    Remove bad migration

--- prisma/migrations/20250925160557_add_embedding_index/migration.sql
+++
@@ -1,3 +0,0 @@
--- Add index on embedding column for better semantic search performance
--- This improves query performance when filtering books with embeddings
-CREATE INDEX IF NOT EXISTS "books_embedding_idx" ON "books"("embedding") WHERE "embedding" IS NOT NULL;
